5be11ee5adbbf9d82565f131af730d82386f0ac1
AdityaS98 <adityasharmalko1998@gmail.com>
2023-07-17 10:46:40 +0530

    Adding a program for comparing strings

---
+++ src/CompareString.java
@@ -0,0 +1,19 @@
+package com.basicprograms;
+
+
+import java.util.Scanner;
+
+public class CompareString {
+    public static void main(String[] args) {
+
+
+        String a = "Lion";
+
+        String b = "Tiger";
+        if (a == b) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
