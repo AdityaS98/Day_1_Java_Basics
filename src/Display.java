8149d28b3d31c7eead679ceb481cf122246e77b3
AdityaS98 <adityasharmalko1998@gmail.com>
2023-07-17 10:49:42 +0530

    Adding a program for leap year

---
+++ src/Display.java
@@ -0,0 +1,8 @@
+package com.basicprograms;
+
+
+public class Display {
+    public static void main(String[] args) {
+        System.out.println("Heloo World");
+    }
+}
---
+++ src/LeapYear.java
@@ -0,0 +1,20 @@
+package com.basicprograms;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year;
+        System.out.println("Enter year");
+         year = sc.nextInt();
+
+         if(year%4==0 && year%400!=0){
+             System.out.println("Leap Year");
+         }
+         else {
+             System.out.println("Not a leap year");
+         }
+
+    }
+}
